910b149823a508f5d9486d52f199b8ecc4fb2120
wasabi-eater <88137260+wasabi-eater@users.noreply.github.com>
2023-07-07 23:26:20 +0900

    Update src.rs
    生成結果の無駄が少なくなった

--- src.rs
+++ src.rs
@@ -1,7 +1,31 @@
+use std::fmt;
+use std::collections::HashMap;
 use std::rc::Rc;
 use core::ops::*;
 use core::cell::Cell;
 use std::marker::PhantomData;
+#[derive(Clone)]
+struct VarID{
+    r: Rc<bool>
+}
+impl VarID{
+    fn new() -> Self {
+        Self{
+            r: Rc::new(true)
+        }
+    }
+}
+impl PartialEq for VarID{
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(&*self.r, &*other.r)
+    }
+}
+impl fmt::Debug for VarID{
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error>{
+        formatter.write_str("VarID")
+    }
+}
+#[derive(Debug)]
 enum ExprTree{
     Num(String),
     Str(String),
@@ -10,39 +34,69 @@ enum ExprTree{
         right: Box<ExprTree>,
         op: &'static str
     },
-    DataItemOf{
-        index: Box<ExprTree>,
-        list: Rc<str>
+    StackVar{
+        var_id: VarID,
+        stack: Rc<str>
     },
-    DataAdd{
-        item: Box<ExprTree>,
-        list: Rc<str>
+    StackPush{
+        var_id: VarID,
+        expr: Box<ExprTree>,
+        stack: Rc<str>
     },
-    DataInsert{
-        item: Box<ExprTree>,
-        index: Box<ExprTree>,
-        list: Rc<str>
-    },
-    DataDelete{
-        index: Box<ExprTree>,
-        list: Rc<str>
-    },
-    DataLengthOf{
-        list: Rc<str>
+    StackDelete{
+        var_id: VarID,
+        stack: Rc<str>
     }
 }
 impl ExprTree{
-    pub fn as_tosh_code(self) -> String{
-        match self {
-            ExprTree::Num(n) => n,
-            ExprTree::Str(s) => format!("\"{}\"", s),
-            ExprTree::BinOp{left, op, right} => format!("({} {} {})", left.as_tosh_code(), op, right.as_tosh_code()),
-            ExprTree::DataItemOf{index, list} => format!("(item {} of {})", index.as_tosh_code(), list),
-            ExprTree::DataAdd{item, list} => format!("add {} to {}", item.as_tosh_code(), list),
-            ExprTree::DataInsert{item, index, list} => format!("insert {} at {} of {}", item.as_tosh_code(), index.as_tosh_code(), list),
-            ExprTree::DataDelete{index, list} => format!("delete {} of {}", index.as_tosh_code(), list),
-            ExprTree::DataLengthOf{list} => format!("(length of {})", list)
+    pub fn as_tosh_code(statements: impl Iterator<Item = ExprTree>) -> String{
+        Self::create_tosh(statements, &mut HashMap::new())
+    }
+    fn create_tosh(statements: impl Iterator<Item = ExprTree>, stacks: &mut HashMap<Rc<str>, Vec<VarID>>) -> String{
+        let mut code: Vec<String> = vec![];
+        for expr in statements{
+            let str = match expr {
+                ExprTree::Num(n) => n,
+                ExprTree::Str(s) => format!("\"{}\"", s),
+                ExprTree::BinOp{left, op, right} => format!("({} {} {})",
+                    Self::create_tosh(vec![*left].into_iter(), stacks),
+                    op,
+                    Self::create_tosh(vec![*right].into_iter(), stacks)),
+                ExprTree::StackVar{var_id, stack: stack_name} => {
+                    let stack = stacks.get(&stack_name).unwrap();
+                    format!("(item {} of {})",
+                        stack.len() - stack.iter().position(|id| *id == var_id).unwrap(),
+                        stack_name
+                    )
+                },
+                ExprTree::StackPush{var_id, stack: stack_name, expr} => {
+                    let insertion = format!("insert {} at 1 of {}",
+                        Self::create_tosh(vec![*expr].into_iter(), stacks),
+                        stack_name
+                    );
+                    let stack = match stacks.get_mut(&stack_name) {
+                        Some(stack) => stack,
+                        None => {
+                            stacks.insert(stack_name.clone(), vec![]);
+                            stacks.get_mut(&stack_name).unwrap()
+                        }
+                    };
+                    stack.push(var_id);
+                    insertion
+                },
+                ExprTree::StackDelete{var_id, stack: stack_name} => {
+                    let stack = stacks.get_mut(&stack_name).unwrap();
+                    let pos = stack.iter().position(|id| *id == var_id).unwrap();
+                    stack.remove(pos);
+                    format!("delete {} of {}",
+                        stack.len() - pos + 1,
+                        stack_name
+                    )
+                }
+            };
+            code.push(str)
         }
+        code.join("\n")
     }
 }
 pub trait FieldSized {
@@ -68,9 +122,12 @@ impl<T1: FieldSized, T2: FieldSized> FieldSized for (T1, T2) {
         T1::size() + T2::size()
     }
 }
+#[derive(Debug)]
 struct StackDelete{
-    stack: Rc<str>
+    stack: Rc<str>,
+    var_id: VarID
 }
+#[derive(Debug)]
 pub struct Expr<'a, T : FieldSized>{
     statements: Vec<ExprTree>,
     post_process: Vec<StackDelete>,
@@ -79,61 +136,36 @@ pub struct Expr<'a, T : FieldSized>{
 }
 impl<'a, T : FieldSized> Expr<'a, T>{
     fn create_tosh(self) -> String {
-        format!("{}\n{}", self.statements.into_iter().map(|s| s.as_tosh_code()).collect::<Vec<_>>().join("\n"),
-            self.post_process.into_iter().map(|s| ExprTree::DataDelete{
-                index: ExprTree::Num("1".into()).into(),
-                list: s.stack
-            }.as_tosh_code()).collect::<Vec<_>>().join("\n"))
+        ExprTree::as_tosh_code(self.statements.into_iter().chain(
+            self.post_process.into_iter().map(|s| ExprTree::StackDelete{
+                var_id: s.var_id,
+                stack: s.stack
+            })))
     }
-    pub fn var<'b, 'c, T2 : FieldSized>(mut self, stack: &'c Stack, f: impl FnOnce(Variable<'c, T>) -> Expr<'b, T2>) -> Expr<'b, T2>
+    pub fn var<'b, 'c, T2 : FieldSized>(self, stack: &'c Stack, f: impl FnOnce(Variable<'c, T>) -> Expr<'b, T2>) -> Expr<'b, T2>
         where T: 'c {
         let expr_count = self.expr.len();
-        let number = (stack.var_count.get() + 1 .. stack.var_count.get() + expr_count + 1).collect();
-        stack.var_count.set(stack.var_count.get() + expr_count);
-        let mut r = f(Variable{stack: &stack, phantom: PhantomData, number});
-        stack.var_count.set(stack.var_count.get() - 1);
+        let var_id: Vec<VarID> = (0..expr_count).map(|_| VarID::new()).collect();
+        let mut r = f(Variable{stack: stack.name.clone(), phantom: PhantomData, var_id: var_id.clone()});
+        
         let mut statements = self.statements;
-        if self.expr.len() == 1 {
-            statements.push(ExprTree::DataInsert{
-                item: self.expr.pop().unwrap().into(),
-                index: ExprTree::Num("0".into()).into(),
-                list: stack.name.clone()});
+        for (expr, var_id) in self.expr.into_iter().zip(var_id.clone()) {
+            statements.push(ExprTree::StackPush{
+                var_id,
+                stack: stack.name.clone(),
+                expr: expr.into()
+            });
         }
-        else {
-            for expr in self.expr.into_iter().rev() {
-                statements.push(ExprTree::DataAdd{item: expr.into(), list: stack.name.clone()});
-            }
-            for _ in 0..expr_count {
-                statements.push(ExprTree::DataInsert{
-                    item: ExprTree::DataItemOf{
-                        index: ExprTree::DataLengthOf{list: stack.name.clone()}.into(),
-                        list: stack.name.clone(),
-                    }.into(),
-                    index: ExprTree::Num("1".into()).into(),
-                    list: stack.name.clone()
-                });
-                statements.push(ExprTree::DataDelete{
-                    index: ExprTree::DataLengthOf{list: stack.name.clone()}.into(),
-                    list: stack.name.clone()
-                });
-            }
-        }
-        for StackDelete{stack: _stack} in self.post_process {
-            if _stack == stack.name {
-                statements.push(ExprTree::DataDelete{
-                    index: ExprTree::Num((expr_count + 1).to_string()).into(),
-                    list: _stack
-                });
-            }
-            else {
-                statements.push(ExprTree::DataDelete{
-                    index: ExprTree::Num("1".into()).into(),
-                    list: _stack
-                });
-            }
+        for StackDelete{stack, var_id} in self.post_process {
+            statements.push(ExprTree::StackDelete{
+                var_id,
+                stack
+            });
         }
         statements.append(&mut r.statements);
-        r.post_process.push(StackDelete{stack: stack.name.clone()});
+        for var_id in var_id {
+            r.post_process.push(StackDelete{stack: stack.name.clone(), var_id});
+        }
         Expr {
             statements,
             post_process: r.post_process,
@@ -302,31 +334,30 @@ impl<'a, 'b> From<&'a str> for Expr<'b, String> {
 }
 
 pub struct Stack{
-    name: Rc<str>,
-    var_count: Cell<usize>
+    name: Rc<str>
 }
 impl Stack{
     pub fn new(name: &str) -> Self {
-        Self {name: name.into(), var_count: Cell::new(0)}
+        Self {name: name.into()}
     }
 }
 
 pub struct Variable<'a, T : FieldSized>{
-    stack: &'a Stack,
+    stack: Rc<str>,
     phantom: PhantomData<&'a T>,
-    number: Vec<usize>
+    var_id: Vec<VarID>
 }
 impl<'a, T : FieldSized> Variable<'a, T> {
     pub fn get(&self) -> Expr<'a, T>{
         Expr{
             statements: vec![],
             post_process: vec![],
-            expr: self.number.iter()
-                .map(|number| ExprTree::DataItemOf{
-                    index: ExprTree::Num((self.stack.var_count.get() - number + 1).to_string()).into(),
-                    list: self.stack.name.clone()
-                })
-                .collect(),
+            expr: self.var_id.iter().map(|var_id|
+                    ExprTree::StackVar{
+                        stack: self.stack.clone(),
+                        var_id: var_id.clone()
+                    }
+                ).collect(),
             phantom: PhantomData
         }
     }
@@ -343,15 +374,16 @@ fn main(){
     let stack = Stack::new("stack");
     let expr = action!{
         stack;
-        let! z = action!{
+        let! x = Expr::from(0.0);
+        let! y = action!{
             stack;
-            let! x = Expr::from(0.4) + Expr::from(3.2);
-            let! y = x.get() + Expr::from(3.0);
-            x.get() * y.get()
+            let! z = x.get() + Expr::from(3.2);
+            let! w = z.get() * Expr::from(2.0);
+            Expr::from((w.get(), "Hey"))
         };
-        let! w = Expr::from(((z.get(), "Hello World"), 3.2));
-        let! w2 = w.get().item0();
+        let! _ = Expr::from((y.get(), "Yeah!"));
         Expr::from(())
     };
+    println!("{:?}", expr);
     println!("{}", expr.create_tosh());
 }
